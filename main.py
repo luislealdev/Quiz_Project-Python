15719ff3aa6dacdf75f69df4d6a25340c7b3ee9f
Luis Leal <luisrrleal@gmail.com>
2023-02-26 20:05:37 -0600

    Finished

--- main.py
+++ main.py
@@ -7,4 +7,8 @@ for question in question_data:
     questions.append(Question(question["text"], question["answer"]))
 
 quiz_brain = QuizBrain(questions)
-quiz_brain.next_question()  
+while quiz_brain.still_has_question():
+    quiz_brain.next_question() 
+
+print("You have completed the quiz!")
+print(f"Your final score is: {quiz_brain.score}/{quiz_brain.question_number}")
